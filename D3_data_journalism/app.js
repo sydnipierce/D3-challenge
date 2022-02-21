bedef22bbdba8532e8fbfa5fb64c6f9c36f65909
Sydni <sydnimpierce@gmail.com>
2022-02-21 12:36:08 -0500

    Set screen width

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -1,15 +1,21 @@
 // Set SVG container dimensions
-var svgWidth = 960;
+var svgWidth = screen.availwidth;
 var svgHeight = 500;
 
 // Prep for chart dimensions
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 100
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
 };
 
 // Set chart dimensions
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);

6664b0fbffddcf662804bf7b13d49ca532d88d6d
Sydni <sydnimpierce@gmail.com>
2022-02-21 12:33:14 -0500

    Start JS code

---
+++ D3_data_journalism/app.js
@@ -0,0 +1,15 @@
+// Set SVG container dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+// Prep for chart dimensions
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+// Set chart dimensions
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
